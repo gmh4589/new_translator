c94bc828f88e0ce0eb3d31304ad5db14690deda4
gmh4589 <41452060+gmh4589@users.noreply.github.com>
2022-12-29 14:21:16 +0300

    Add files via upload

--- new_translator.py
+++ new_translator.py
@@ -2,6 +2,7 @@
 from kivy.app import App
 from kivy.factory import Factory
 from kivy.lang import Builder
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
 from tkinter import filedialog as fd
 
@@ -16,6 +17,9 @@ class MainScreen(Screen):
 
     values = []
     filename = ''
+    sheet = ''
+    readData = ''
+    sss = 0
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -35,59 +39,65 @@ class MainScreen(Screen):
         print(self.filename)
 
         if self.filename != '':
-            rearData = openpyxl.load_workbook(self.filename, data_only = True)
-            sheet = rearData.active
-            rows = sheet.max_row
-            cols = sheet.max_column
+            self.readData = openpyxl.load_workbook(self.filename, data_only = True)
+            self.sheet = self.readData.active
+            rows = self.sheet.max_row
+            cols = self.sheet.max_column
 
             for row in range(1, rows):
                 for col in range(1, cols):
-                    cell = sheet.cell(row = row, column = col).value
+                    cell = self.sheet.cell(row = row, column = col).value
+
                     if cell == 'dialog':
-                        val = [sheet.cell(row = row + 1, column = col).value, '', row + 1, col]
+                        val = [self.sheet.cell(row = row + 1, column = col).value, '', row + 1, col]
                         self.values.append(val)
 
             print(self.values)
 
             self.ids['originalTXT'].text = str(self.values[0][0])
             self.ids['newTXT'].text = str(self.values[0][0])
-            self.ids['rdyLabel'].text = '1/' + str(len(self.values) - 1) + ' готово'
-            self.ids['longLabel'].text = str(len(self.values[0][1])) + ' символов'
+            self.ids['rdyLabel'].text = '1/' + str(len(self.values)) + ' готово'
+            self.ids['longLabel'].text = str(len(self.values[0][0])) + ' символов'
             self.ids['percentLabel'].text = str((100 / len(self.values) * sss + 1))[:4] + ' %'
+
+    def writeFile(self):
+
+        for cell in range(len(self.values)):
+            self.sheet.cell(row = int(self.values[cell][2]),
+                            column = int(self.values[cell][3])).value\
+                = self.values[cell][1]
+
+        self.readData.save(self.filename)
+        Factory.SavedPopup().open()
 
     def nextString(self, step = '+'):
 
         global sss
-        global values
+
+        def setString():
+            self.values[sss][1] = self.ids['newTXT'].text
+            print(self.values[sss][1])
 
         try:
-            if step == '+':
-                if self.ids['originalTXT'].text != self.ids['newTXT'].text:
-                    self.values[sss][1] = self.ids['newTXT'].text
-                sss += 1
-            elif step == '-' :
-                if sss != -1:
-                    if self.ids['originalTXT'].text == self.ids['newTXT'].text:
-                        self.values[sss][1] = self.ids['newTXT'].text
-                else:
-                    Factory.EOFPopup().open()
-                    sss = 0
-                sss -= 1
+            setString()
+            if step == '+': sss += 1
+            elif step == '-':
+                if sss != 0: sss -= 1
+                else: Factory.EOFPopup().open()
 
-            print(self.values)
+            #print(self.values)
 
             self.ids['originalTXT'].text = str(self.values[sss][0])
             if str(self.values[sss][1]) == '': self.ids['newTXT'].text = str(self.values[sss][0])
             else: self.ids['newTXT'].text = str(self.values[sss][1])
             self.ids['rdyLabel'].text = str(sss + 1) + '/' + str(len(self.values)) + ' готово'
-            self.ids['longLabel'].text = str(len(self.values[sss][1])) + ' символов'
+            self.ids['longLabel'].text = str(len(self.values[sss][0])) + ' символов'
             self.ids['percentLabel'].text = str((100 / len(self.values) * sss))[:4] + ' %'
+
         except IndexError: Factory.EOFPopup().open()
 
     def reFresh(self):
         self.ids['newTXT'].text = self.ids['originalTXT'].text
-
-
 
 class NewTranslatorApp(App):
 
